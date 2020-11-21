d87d537e98d920fb68adc19814401c947ca93815
Denis Kotlica <neosiae@protonmail.com>
2020-11-21 17:48:24 +0100

    Add another media query

--- src/components/Jobs/styles.js
+++ src/components/Jobs/styles.js
@@ -3,8 +3,11 @@ import styled from 'styled-components'
 export const Container = styled.div`
   margin-top: 3.5rem;
   padding: 0 1.5rem;
-  @media (min-width: 1440px) {
+  @media (min-width: 992px) {
     margin-top: 5rem;
+    margin-bottom: 8rem;
+  }
+  @media (min-width: 1440px) {
     padding: 0 5.5rem;
   }
 `
